c7f612848db2e6a375361aba97bf61bb8ab5e646
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2023-04-17 15:22:53 +0530

    env ignored and name is added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000 ;
 
 
 app.get('/', (req, res) => {
-    res.send("Appllication is running " + port)
+    res.send("Appllication is running " + port + " The name is" + " "+ process.env.NAME )
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
